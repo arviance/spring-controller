09e8eb7da9ed87ce00c0d6a2e873aa10dbbd4c4e
Kamran Dalir <k.dalir@gmail.com>
2021-05-19 20:11:06 +0200

    merge

--- src/main/java/com/home/controllers/BroController.java
+++ src/main/java/com/home/controllers/BroController.java
@@ -11,9 +11,12 @@ public class BroController {
     public String giveCricketBat() {
         return "MRFCricketbat";
     }
+<<<<<<< HEAD
 
     //testing git revert
 
 
     //test 2 git revert
+=======
+>>>>>>> parent of d500ab7... testing git revert 1
 }
